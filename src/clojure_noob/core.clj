bdbc8d22e572d212db9476ba5dd4964b89d71bd0
MSh <micangel@mail.ru>
2021-09-09 15:42:09 +0300

    Expr 1.8.

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -19,13 +19,13 @@
 (def good-enough? (fn [guess x]
                     (< (abs (- (square guess) x)) 0.001)))
 
-(def good? (fn [guess x]
-             (<= (- (improve guess x) guess) 0.0001)))
-
 (def average (fn [x y] (/ (+ x y) 2)))
 
 (def improve (fn [guess x]
                (average guess (/ x guess))))
+
+(def good? (fn [guess x]
+             (<= (- (improve guess x) guess) 0.0001)))
 
 (def sqrt-iter (fn [guess x]
                  (if (good-enough? guess x)
@@ -33,3 +33,18 @@
                    (sqrt-iter (improve guess x) x))))
 
 (def sqrt (fn [x] (sqrt-iter 1.0 x)))
+;=====1.8======
+(defn cube [x] (* x x x))
+
+(defn improve-cube [guess x]
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(defn good-enough-cube? [guess x]
+  (< (abs (- (cube guess) x)) 0.001))
+
+(defn cube-iter [guess x]
+  (if (good-enough-cube? guess x)
+    guess
+    (cube-iter (improve-cube guess x) x)))
+
+(defn cube-sqrt [x] (cube-iter 1.0 x))
